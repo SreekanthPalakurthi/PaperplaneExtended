b23dda3604b0dc2f1e5508a3f1ded9e2e1fec498
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2021-03-15 15:41:41 +0530

    tone down sys stats

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -40,88 +40,6 @@ async def sysdetails(sysd):
         except FileNotFoundError:
             await sysd.edit("**Install neofetch first!**")
 
-
-@register(outgoing=True, pattern=r"^\.botver$")
-async def bot_ver(event):
-    """ For .botver command, get the bot version. """
-    if event.text[0].isalpha() or event.text[0] in ("/", "#", "@", "!"):
-        return
-    if which("git") is not None:
-        ver = await asyncrunapp(
-            "git",
-            "describe",
-            "--all",
-            "--long",
-            stdout=asyncPIPE,
-            stderr=asyncPIPE,
-        )
-        stdout, stderr = await ver.communicate()
-        verout = str(stdout.decode().strip()) + str(stderr.decode().strip())
-
-        rev = await asyncrunapp(
-            "git",
-            "rev-list",
-            "--all",
-            "--count",
-            stdout=asyncPIPE,
-            stderr=asyncPIPE,
-        )
-        stdout, stderr = await rev.communicate()
-        revout = str(stdout.decode().strip()) + str(stderr.decode().strip())
-
-        await event.edit(f"**Userbot:** `{verout}`\n" f"**Revision:** `{revout}`\n")
-    else:
-        await event.edit("**Shame that you don't have git!**")
-
-
-@register(outgoing=True, pattern=r"^\.pip(?: |$)(.*)")
-async def pipcheck(pip):
-    """ For .pip command, do a pip search. """
-    if pip.text[0].isalpha() or pip.text[0] in ("/", "#", "@", "!"):
-        return
-    pipmodule = pip.pattern_match.group(1)
-    if pipmodule:
-        await pip.edit("**Searching...**")
-        pipc = await asyncrunapp(
-            "pip3",
-            "search",
-            pipmodule,
-            stdout=asyncPIPE,
-            stderr=asyncPIPE,
-        )
-
-        stdout, stderr = await pipc.communicate()
-        pipout = str(stdout.decode().strip()) + str(stderr.decode().strip())
-
-        if pipout:
-            if len(pipout) > 4096:
-                await pip.edit("**Output too large, sending as file...**")
-                with open("output.txt", "w+") as file:
-                    file.write(pipout)
-                await pip.client.send_file(
-                    pip.chat_id,
-                    "output.txt",
-                    reply_to=pip.id,
-                )
-                remove("output.txt")
-                return
-            await pip.edit(
-                "**Query: **\n`"
-                f"pip3 search {pipmodule}"
-                "`\n**Result: **\n`"
-                f"{pipout}"
-                "`"
-            )
-        else:
-            await pip.edit(
-                "**Query: **\n`"
-                f"pip3 search {pipmodule}"
-                "`\n**Result: **\n`No result returned/False`"
-            )
-    else:
-        await pip.edit("**Use .help pip to see an example.**")
-
-
 @register(outgoing=True, pattern=r"^\.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
@@ -155,8 +73,6 @@ async def amireallyalivereset(ureset):
 CMD_HELP.update(
     {
         "sysd": ">`.sysd`" "\nUsage: Shows system information using neofetch.",
-        "botver": ">`.botver`" "\nUsage: Shows the userbot version.",
-        "pip": ">`.pip <module(s)>`" "\nUsage: Does a search of pip modules(s).",
         "alive": ">`.alive`"
         "\nUsage: Type .alive to see wether your bot is working or not."
         "\n\n>`.aliveu <text>`"
