779a2216832591a2a0ea3c1f7ec5f07ed324b6d7
KenHV <yo@kenharris.xyz>
2021-04-06 21:58:28 +0530

    kensurbot: v1.2
    Change-Id: Ia28ec0cfc69a672ffc7b41375ed09c77bd41da81
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -163,7 +163,7 @@ GENIUS = os.environ.get("GENIUS_ACCESS_TOKEN") or None
 USR_TOKEN = os.environ.get("USR_TOKEN_UPTOBOX") or None
 
 # KensurBot version
-KENSURBOTMOD_VERSION = "1.1"
+KENSURBOTMOD_VERSION = "1.2"
 
 # 'bot' variable
 if STRING_SESSION:
