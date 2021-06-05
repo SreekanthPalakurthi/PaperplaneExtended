KenHV/KensurBot

kensurbot: update trigger message with alive status if bot restarts
before restarting, the chat ID and message ID is stored in a file.
when the bot starts, it'll check if that file exists, and retrieve the
contents if it does. it'll be used to edit the "restarting" message.

this is much better than sending a message to botlog everytime the bot
starts.

Change-Id: I60b780caffdb02b3f748863c5e22efaff48f55e8
Signed-off-by: KenHV <yo@kenharris.xyz>

userbot/__init__.py
@@ -222,24 +222,27 @@ with bot:
         sys.exit(1)
 
 
-async def send_alive_status():
-    if BOTLOG_CHATID and LOGSPAMMER:
-        DEFAULTUSER = ALIVE_NAME or "Set `ALIVE_NAME` ConfigVar!"
-        message = (
-            f"**KensurBot v{KENSURBOT_VERSION} is up and running!**\n\n"
-            f"**Telethon:** {version.__version__}\n"
-            f"**Python:** {python_version()}\n"
-            f"**User:** {DEFAULTUSER}"
-        )
-        await bot.send_message(BOTLOG_CHATID, message)
-        return True
+async def update_restart_msg(chat_id, msg_id):
+    DEFAULTUSER = ALIVE_NAME or "Set `ALIVE_NAME` ConfigVar!"
+    message = (
+        f"**KensurBot v{KENSURBOT_VERSION} is up and running!**\n\n"
+        f"**Telethon:** {version.__version__}\n"
+        f"**Python:** {python_version()}\n"
+        f"**User:** {DEFAULTUSER}"
+    )
+    await bot.edit_message(chat_id, msg_id, message)
+    return True
 
 
-with bot:
-    try:
-        bot.loop.run_until_complete(send_alive_status())
-    except:
-        pass
+if os.path.isfile(".restartmsg"):
+    with open(".restartmsg") as f:
+        chat_id, msg_id = map(int, f)
+    with bot:
+        try:
+            bot.loop.run_until_complete(update_restart_msg(chat_id, msg_id))
+        except:
+            pass
+    os.remove(".restartmsg")
 
 # Global Variables
 COUNT_MSG = 0

SreekanthPalakurthi/PaperplaneExtended

userbot/__init__.py
@@ -216,24 +216,28 @@ with bot:
         sys.exit(1)
 
 
-async def send_alive_status():
-    if BOTLOG_CHATID and LOGSPAMMER:
-        DEFAULTUSER = ALIVE_NAME or "Set `ALIVE_NAME` ConfigVar!"
-        message = (
-            f"**KensurBotMod v{KENSURBOTMOD_VERSION} is up and running!**\n\n"
-            f"**Telethon:** {version.__version__}\n"
-            f"**Python:** {python_version()}\n"
-            f"**User:** {DEFAULTUSER}"
-        )
-        await bot.send_message(BOTLOG_CHATID, message)
-        return True
+async def update_restart_msg(chat_id, msg_id):
+    DEFAULTUSER = ALIVE_NAME or "Set `ALIVE_NAME` ConfigVar!"
+    message = (
+        f"**KensurBot v{KENSURBOTMOD_VERSION} is up and running!**\n\n"
+        f"**Telethon:** {version.__version__}\n"
+        f"**Python:** {python_version()}\n"
+        f"**User:** {DEFAULTUSER}"
+    )
+    await bot.edit_message(chat_id, msg_id, message)
+    return True
 
 
-with bot:
-    try:
-        bot.loop.run_until_complete(send_alive_status())
-    except:
-        pass
+if os.path.isfile(".restartmsg"):
+    with open(".restartmsg") as f:
+        chat_id, msg_id = map(int, f)
+    with bot:
+        try:
+            bot.loop.run_until_complete(update_restart_msg(chat_id, msg_id))
+        except:
+            pass
+    os.remove(".restartmsg")
+
 
 # Global Variables
 COUNT_MSG = 0